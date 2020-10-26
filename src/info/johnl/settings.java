5d45a22b0a854f05a82a991a1674d579a2af32b0
Nox-Mob <53887598+Nox-Mob@users.noreply.github.com>
2020-10-26 15:28:43 -0500

    updated settings button names
    Also added strings to declare the paths to launch the settings pages

--- src/info/johnl/settings.java
+++ src/info/johnl/settings.java
@@ -1,17 +1,25 @@
 package info.johnl;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URI;
 
 
 public class settings {
-    private JButton selectFileLocationButton3;
+    private JButton selectVATISFileLocationButton;
     private JPanel settingsPanel;
-    private JButton selectFileLocationButton2;
-    private JButton selectFileLocationButton1;
-    private JButton selectFileLocationButton;
+    private JButton selectVERAMFileLocationButton;
+    private JButton selectVSTARSFileLocationButton;
+    private JButton selectVRCFileLocationButton;
     private JButton saveAndExitButton;
+
+    String userprofile = System.getenv("USERPROFILE");
+    String vrcPath = "C:\\Program Files (x86)\\VRC\\VRC.exe";
+    String vstarsPath = "C:\\Program Files (x86)\\vSTARS\\vSTARS.exe";
+    String veramPath = userprofile + "\\AppData\\Local\\vERAM\\vERAM.exe";
+    String vatisPath = userprofile + "\\AppData\\Local\\vATIS\\vATIS.exe";
 
     public static JFrame frame = new JFrame("Settings");
 
@@ -31,6 +39,17 @@ public class settings {
             }
         });
     }
+
+        selectVRCFileLocationButton.addActionListener(new ActionListener();
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Runtime.getRuntime().exec("explorer.exe /select," + vrcPath);
+            }
+    )};
+
+
+
+    public JButton
 
     //TODO add function to the file selector
     //TODO style settings panel
